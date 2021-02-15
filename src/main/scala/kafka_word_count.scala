eb95f9191a5e9f8a1154d8b8f18193b5f984ee22
Daniel Stahl <danstahl1138@gmail.com>
2021-02-15 15:15:31 -0600

    revamped folders and converted to structured streaming

--- src/main/scala/kafka_word_count.scala
+++ src/main/scala/kafka_word_count.scala
@@ -17,14 +17,32 @@
 
 // scalastyle:off println
 package dhstest
+import org.apache.log4j.{Level, Logger}
 
+import org.apache.spark.internal.Logging
+
+/** Utility functions for Spark Streaming examples. */
+object StreamingExamples extends Logging {
+
+  /** Set reasonable logging levels for streaming if the user has not configured log4j. */
+  def setStreamingLogLevels(): Unit = {
+    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+    if (!log4jInitialized) {
+      // We first log something to initialize Spark's default logging, then we override the
+      // logging level.
+      logInfo("Setting log level to [WARN] for streaming example." +
+        " To override add a custom log4j.properties to the classpath.")
+      Logger.getRootLogger.setLevel(Level.WARN)
+    }
+  }
+}
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
-
+import org.apache.spark.sql.SparkSession
 /**
  * Consumes messages from one or more topics in Kafka and does wordcount.
  * Usage: DirectKafkaWordCount <brokers> <topics>
@@ -49,34 +67,35 @@ object DirectKafkaWordCount {
       System.exit(1)
     }
 
-    //StreamingExamples.setStreamingLogLevels()
+    StreamingExamples.setStreamingLogLevels()
 
     val Array(brokers, groupId, topics) = args
     // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
-
+    //val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
+    val spark = SparkSession
+      .builder
+      .appName("DirectKafkaWordCount")
+      .getOrCreate()
+    //val ssc = new StreamingContext(sparkConf, Seconds(2))
+    //val topicsSet = topics.split(",").toSet
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", brokers)
+      .option("subscribe", topics)
+      .load()
+    
+    val result=df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING) as value", "1 as groupTest").groupBy("groupTest").count()
+   
+    result.writeStream
+      .format("console")
+      .outputMode("Complete")
+      .option("truncate","false")
+      .start()
+      .awaitTermination()
     // Create direct kafka stream with brokers and topics
-    val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, Object](
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
-      ConsumerConfig.GROUP_ID_CONFIG -> groupId,
-      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer])
-    val messages = KafkaUtils.createDirectStream[String, String](
-      ssc,
-      LocationStrategies.PreferConsistent,
-      ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams))
-
-    // Get the lines, split them into words, count the words and print
-    val lines = messages.map(_.value)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
-
-    // Start the computation
-    ssc.start()
-    ssc.awaitTermination()
+    
+    
   }
 }
 // scalastyle:on println
