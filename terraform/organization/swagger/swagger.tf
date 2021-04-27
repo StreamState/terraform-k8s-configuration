f1bc5bf4d2d687e2bdd04de182548f154d44fa3a
Daniel Stahl <danstahl1138@gmail.com>
2021-04-27 08:44:48 -0500

    added swagger to tf

--- terraform/organization/swagger/swagger.tf
+++ terraform/organization/swagger/swagger.tf
@@ -1,11 +1,17 @@
+locals {
+  service_name = "streamstate-api.endpoints.${var.project}.cloud.goog"
+}
+
+
 resource "google_endpoints_service" "openapi_service" {
-  service_name = local.realoptions_gateway_url
+  service_name = local.service_name
   project      = var.project
   openapi_config = templatefile(
     "../../swagger/openapi.yml",
     {
       PROJECT_ID = var.project
       CLUSTER_IP = var.clusterip
+      HOST       = local.service_name
     }
   )
 
