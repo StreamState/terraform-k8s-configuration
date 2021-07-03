ee98b3882beeab6a54eb3d2d6f4fc9b96f83dd15
Daniel Stahl <danstahl1138@gmail.com>
2021-07-03 09:38:20 -0500

    cleanup

--- adminapp/index.js
+++ adminapp/index.js
@@ -3,67 +3,23 @@ const https = require('https')
 const kc = new k8s.KubeConfig()
 kc.loadFromDefault()
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
-
+const {createNewSecret}=require('./utils')
 //const k8sApiCustomObject = kc.makeApiClient(k8s.CustomObjectsApi)
 
 const fastify = require('fastify')()
 
 const path = require('path')
-const { generateNewSecret, createNewSecret, getWriteToken}=require('./utils')
 fastify.register(require('fastify-static'), {
     root: path.join(__dirname, 'public'),
     prefix: '/public/', // optional: default '/'
 })
 fastify.register(require('fastify-formbody'))
 const {
-    WRITE_TOKEN_NAME: writeTokenName,
-    READ_TOKEN_NAME: readTokenName,
     CONFLUENT_KEY_NAME: confluentKeyName,
     CONFLUENT_SECRET_NAME: confluentSecretName,
     NAMESPACE: namespace,
-    HOST: host
 } = process.env
 
-fastify.get('/applications', (req, reply)=>{
-    return getWriteToken().then(token=>{
-        const options = {
-            hostname: host,
-            port: 443,
-            path: '/api/applications',
-            method: 'GET',
-            headers:{
-                Authorization: `Bearer ${token}`
-            }
-        }
-        let chunk=''
-        https.request(options, res=>{
-            res.on('data', d => {
-                chunk+=d
-            })
-            res.on('end', ()=>{
-                reply.send(JSON.parse(chunk))
-            })
-        }).on('error', e => {
-            reply.send({ error: e.message })
-        })
-    })
-})
-fastify.post('/rotate/write', (req, reply) => {
-    generateNewSecret(writeTokenName, k8s, k8sApi, namespace)
-        .then(secret => reply.send({ secret }))
-        .catch(e => {
-            console.log(e)
-            reply.send({ error: e.message })
-        })
-})
-fastify.post('/rotate/read', (req, reply) => {
-    generateNewSecret(readTokenName, k8s, k8sApi, namespace)
-        .then(secret => reply.send({ secret }))
-        .catch(e => {
-            console.log(e)
-            reply.send({ error: e.message })
-        })
-})
 fastify.post('/confluent/create', (req, reply) => {
     return Promise.all([
         createNewSecret(confluentKeyName, req.body.confluentKey, k8s, k8sApi,namespace, 'key'),
