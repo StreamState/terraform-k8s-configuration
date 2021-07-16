912544699a6a6397960936358103f229038b3a49
Daniel Stahl <danstahl1138@gmail.com>
2021-07-16 13:48:27 -0500

    removed docker

--- docker/streamstate_scripts/create_folder.py
+++
@@ -1,9 +0,0 @@
-from streamstate_utils.gcs_create_folder import create_gcs_folders
-from streamstate_utils.structs import InputStruct
-import sys
-import json
-
-if __name__ == "__main__":
-    [_, app_name, bucket_name, input_struct] = sys.argv
-    input_info = [InputStruct(**v) for v in json.loads(input_struct)]
-    create_gcs_folders(bucket_name, app_name, input_info)
